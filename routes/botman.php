93621d5a44db12da7e0ce432a8a1206c10f7c953
KraIsrael <jacob.kra@inpulsion.net>
2018-08-14 17:40:07 +0000

    route

--- routes/botman.php
+++ routes/botman.php
@@ -1,9 +1,45 @@
 <?php
 use App\Http\Controllers\BotManController;
+use App\Conversations\ListComputerConv;
+use BotMan\BotMan\Middleware\ApiAi;
+
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+$dialogflow = ApiAi::create('2a9e73da4eb047de9ab9d9e90ed5b2f6')->listenForAction();
+// Apply global "received" middleware
+$botman->middleware->received($dialogflow);
+// Apply matching middleware per hears command
+$botman->hears('hellofrom dialogflow', function (\BotMan\BotMan\BotMan $bot) {
+    // The incoming message matched the "my_api_action" on Dialogflow
+    // Retrieve Dialogflow information:
+    /* $extras = $bot->getMessage()->getExtras();
+     $apiReply = $extras['apiReply'];
+     $apiAction = $extras['apiAction'];
+     $apiIntent = $extras['apiIntent']; */
+
+    $bot->reply("Connexion ok avec dialogflow");
+})->middleware($dialogflow);
+
+// Apply global "received" middleware
+$botman->middleware->received($dialogflow);
+
+
+$botman->hears('Je suis {name}', function ($bot, $name) {
+    $bot->reply('Bienvenu  '.$name);
 });
+
+
+
+$botman->hears(
+    'Je cherche un ordinateur portable',
+    function ($bot){
+        $bot->startConversation(new ListComputerConv());
+    }
+);
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+$botman->hears('Ordinateur portable', function ($bot){
+    $message = Message::create('Nous avons une promotion en cours')
+        ->image('https://inpulsclic.s3.us-west-2.amazonaws.com/customer-logo/SNGI_logo.jpg');
+    $bot->reply($message);
+});
--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,19 @@
 <?php
-use App\Http\Controllers\BotManController;
-use App\Conversations\ListComputerConv;
-use BotMan\BotMan\Middleware\ApiAi;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-$botman = resolve('botman');
-
-$dialogflow = ApiAi::create('2a9e73da4eb047de9ab9d9e90ed5b2f6')->listenForAction();
-// Apply global "received" middleware
-$botman->middleware->received($dialogflow);
-// Apply matching middleware per hears command
-$botman->hears('hellofrom dialogflow', function (\BotMan\BotMan\BotMan $bot) {
-    // The incoming message matched the "my_api_action" on Dialogflow
-    // Retrieve Dialogflow information:
-    /* $extras = $bot->getMessage()->getExtras();
-     $apiReply = $extras['apiReply'];
-     $apiAction = $extras['apiAction'];
-     $apiIntent = $extras['apiIntent']; */
-
-    $bot->reply("Connexion ok avec dialogflow");
-})->middleware($dialogflow);
-
-// Apply global "received" middleware
-$botman->middleware->received($dialogflow);
-
-
-$botman->hears('Je suis {name}', function ($bot, $name) {
-    $bot->reply('Bienvenu  '.$name);
+Route::get('/', function () {
+    return view('welcome');
 });
 
-
-
-$botman->hears(
-    'Je cherche un ordinateur portable',
-    function ($bot){
-        $bot->startConversation(new ListComputerConv());
-    }
-);
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
-$botman->hears('Ordinateur portable', function ($bot){
-    $message = Message::create('Nous avons une promotion en cours')
-        ->image('https://inpulsclic.s3.us-west-2.amazonaws.com/customer-logo/SNGI_logo.jpg');
-    $bot->reply($message);
-});
+Route::match(['get', 'post'], '/botman', 'BotManController@handle');
+Route::get('/botman/tinker', 'BotManController@tinker');
